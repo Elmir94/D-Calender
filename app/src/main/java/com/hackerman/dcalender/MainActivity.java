191e1621001b3f0c084d2869146297114ebbe0ae
Iglosen <jonte.birk@hotmail.com>
2019-11-14 13:38:02 +0100

    Changed labels

--- app/src/main/java/com/hackerman/dcalender/MainActivity.java
+++ app/src/main/java/com/hackerman/dcalender/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Eksdi", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
