ccf564e2714a7dce3e0ccdf850f7ca3dff7907f6
Iglosen <Jonte.birk@hotmail.com>
2019-11-23 08:54:05 +0100

    Added labels and color schemes, working on implementing the tasks

--- app/src/main/java/com/hackerman/dcalender/MainActivity.java
+++ app/src/main/java/com/hackerman/dcalender/MainActivity.java
@@ -1,20 +1,32 @@
 package com.hackerman.dcalender;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.schedule);
+    public void onTaskClick(View view)
+    {
+        Snackbar clickMessage = Snackbar.make(view, "Redirected to deep task view", Snackbar.LENGTH_SHORT);
+        clickMessage.show();
+    }
 
+    public void openSidebar(View view)
+    {
+        Snackbar clickMessage = Snackbar.make(view, "Opening sidebar", Snackbar.LENGTH_SHORT);
+        clickMessage.show();
+    }
 
+    private void setupTimestamps(){
         String[] timeStamps;
         timeStamps = new String[24];
         for (int i = 0; i < timeStamps.length; i++) {
@@ -27,14 +39,56 @@ public class MainActivity extends AppCompatActivity {
         }
 
         LinearLayout timeStampsView = (LinearLayout) findViewById(R.id.timeStamps);
+        LinearLayout timeStampLinesView = (LinearLayout) findViewById(R.id.timeStampLines);
+
+        View addMargin = new View(this);
+        addMargin.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,75));
+        timeStampsView.addView(addMargin);
+
+        View addMarginLines = new View(this);
+        addMarginLines.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,75));
+        timeStampLinesView.addView(addMarginLines);
+
+
         for( int i = 0; i < timeStamps.length; i++ )
         {
             TextView textView = new TextView(this);
             textView.setText(timeStamps[i]);
-            textView.setGravity(Gravity.RIGHT);
+            textView.setGravity(Gravity.TOP | Gravity.END);
             textView.setHeight(150);
+            textView.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
             timeStampsView.addView(textView);
+
+            View line = new View(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,5));
+            params.setMargins(10,25,0,120);
+            line.setLayoutParams(params);
+            line.setBackgroundColor(Color.DKGRAY);
+            timeStampLinesView.addView(line);
         }
+
+        View addBottomMargin = new View(this);
+        addBottomMargin.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,97));
+        timeStampsView.addView(addBottomMargin);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule);
+
+        setupTimestamps();
+
+        ScrollView mScrollView = (ScrollView) findViewById(R.id.verticalScroll);
+        mScrollView.post(new Runnable() {
+            public void run() {
+                ScrollView mScrollView = (ScrollView) findViewById(R.id.verticalScroll);
+                mScrollView.scrollTo(0, 1200);
+            }
+        });
+        //Add tasks
+        LinearLayout day1View = (LinearLayout) findViewById(R.id.daySchedule1);
 
 
 
